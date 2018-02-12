d5f5d21206840c2832cd02e27495b6331dd873d8
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-02-12 18:28:37 +0700

    :tada: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { createStore } from "redux"
+
+type Action =
+    | { type: "Increment" }
+    | { type: "Decrement" }
+
+function counter(state = 0, action: Action) {
+    switch (action.type) {
+        case "Increment": {
+            return state + 1
+        }
+        case "Decrement": {
+            return state - 1
+        }
+    }
+}
+
+let store = createStore(counter);
+
+store.dispatch({ type: "Increment" })
+store.dispatch({ type: "Increment" })
+store.dispatch({ type: "Increment" })
+store.dispatch({ type: "Increment" })
+store.dispatch({ type: "Increment" })
+store.dispatch({ type: "Increment" })
+store.dispatch({ type: "Decrement" })
+
+console.log(store.getState());
